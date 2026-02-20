226ae96f3a5bd69065aa9d8170df4f51470719a6
Ayyan <ayyanamir6@gmail.com>
2026-02-20 12:28:08 +0500

    fix: resolve cursor bot reported isssues

--- users/views.py
+++ users/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.password_validation import validate_password
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 
-# Create your views here.
+
 def signup_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        
-        User.objects.create_user(
-            username=username,
-            password=password
-        )
-        
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not username or not password:
+            messages.error(request, 'Username and password are required.')
+            return render(request, 'users/signup.html')
+
+        user = User(username=username)
+        try:
+            validate_password(password, user=user)
+        except ValidationError as validation_errors:
+            for error in validation_errors:
+                messages.error(request, error)
+            return render(request, 'users/signup.html')
+
+        try:
+            User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            messages.error(request, 'Username is already taken. Please choose a different one.')
+            return render(request, 'users/signup.html')
+
         return redirect('login')
-    
+
     return render(request, 'users/signup.html')
         
 
