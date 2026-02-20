0610b406b2a7ba6435a82d7684f7ed453b72e707
Ayyan <ayyanamir6@gmail.com>
2026-02-20 14:49:36 +0500

    fix: add @require_POST decorator to logout_view for improved request handling

--- users/views.py
+++ users/views.py
@@ -52,8 +52,8 @@ def login_view(request):
     
     return render(request, 'users/login.html')
 
-@require_POST
 @login_required
+@require_POST
 def logout_view(request):
     logout(request)
     return redirect('login')
